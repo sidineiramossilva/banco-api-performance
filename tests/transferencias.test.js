9b0354c2d45baa001374fb43dcf4f0091484b2f5
Sidinei <sydney.ramos@hotmail.com>
2025-09-27 23:51:04 -0300

    Criei um arquivo de configuração e apliquei variáveis de ambiente

--- tests/transferencias.test.js
+++ tests/transferencias.test.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { obterToken } from '../helpers/autenticacao.js';
+import { pegarBaseURL } from '../utils/variaveis.js';
 
 export const options = {
     iterations: 1
@@ -9,7 +10,7 @@ export const options = {
 export default function() {
     const token = obterToken()
 
-    const url = 'http://localhost:3000/transferencias';
+    const url = pegarBaseURL() + '/transferencias';
 
     const payload = JSON.stringify({
         contaOrigem: 1,
@@ -30,6 +31,6 @@ export default function() {
     check(res, {
         "status is 201": (r) => r.status === 201 
     });
-    
+
     sleep(1);
 }
